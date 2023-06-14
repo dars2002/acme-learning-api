2abb576928b391d195e97f5f5b145fd7a5a843a6
David Rivas <u20191e831@upc.edu.pe>
2023-06-13 21:35:29 -0500

    feat: added user event creation.

--- src/iam/domain/model/aggregates/user.entity.ts
+++ src/iam/domain/model/aggregates/user.entity.ts
@@ -1,6 +1,7 @@
 import { AggregateRoot } from '@nestjs/cqrs';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { EmailAddress } from '../value-objects/email-address.value-object';
+import { UserCreatedEvent } from '../events/user-created.event';
 
 @Entity('users')
 export class User extends AggregateRoot {
@@ -15,5 +16,9 @@ export class User extends AggregateRoot {
 
   constructor() {
     super();
+  }
+
+  onCreation(): void {
+    this.apply(new UserCreatedEvent(this.id));
   }
 }
